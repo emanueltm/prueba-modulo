a905a0ac70116fe64f7c8b2114ddf593ab7fd825
emanueltm <emanuel.torres.m@hotmail.com>
2025-05-07 15:11:48 -0600

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,4 +10,3 @@ $routes->get('/', 'Usuario\Login::index');
 $routes->get('login', 'Usuario\Login::index');
 $routes->post('validar_usuario', 'Usuario\Login::validar_datos');
 $routes->get('dashboard', 'Usuario\Dashboard::index');
-
